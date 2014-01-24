058a119a9ab19c92554eb5c3aeac4e09626d648d
Arnav Gupta <championswimmer@aokp.co>
2014-01-24 21:29:45 +0530

    create entry points to all fragments
     TODO :
     Conditionally NOT include some fragments if they are not required
    
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- app/src/main/java/com/teamosc/collegefests/MainActivity.java
+++ app/src/main/java/com/teamosc/collegefests/MainActivity.java
@@ -1,6 +1,7 @@
 package com.teamosc.collegefests;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -18,8 +19,21 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.teamosc.collegefests.fragments.AboutFragment;
+import com.teamosc.collegefests.fragments.ContactFragment;
+import com.teamosc.collegefests.fragments.EventsFragment;
+import com.teamosc.collegefests.fragments.RegisterFragment;
+import com.teamosc.collegefests.fragments.UpdatesFragment;
+
 public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements
+        NavigationDrawerFragment.NavigationDrawerCallbacks,
+        AboutFragment.OnFragmentInteractionListener,
+        ContactFragment.OnFragmentInteractionListener,
+        EventsFragment.OnFragmentInteractionListener,
+        RegisterFragment.OnFragmentInteractionListener,
+        UpdatesFragment.OnFragmentInteractionListener
+{
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -50,23 +64,22 @@ public class MainActivity extends ActionBarActivity
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment showFragment = AboutFragment.newInstance("a", "a");
+        switch (position){
+            case 0: showFragment = AboutFragment.newInstance("a", "a"); break;
+            case 1: showFragment = EventsFragment.newInstance("a", "a"); break;
+            case 2: showFragment = UpdatesFragment.newInstance("a", "a"); break;
+            case 3: showFragment = RegisterFragment.newInstance("a", "a"); break;
+            case 4: showFragment = ContactFragment.newInstance("a", "a"); break;
+        }
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, showFragment)
                 .commit();
     }
 
     public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
+        String[] drawerItems = getResources().getStringArray(R.array.drawer_items);
+        mTitle = drawerItems[number-1];
     }
 
     public void restoreActionBar() {
@@ -102,46 +115,8 @@ public class MainActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
-
 }
--- app/src/main/java/com/teamosc/collegefests/NavigationDrawerFragment.java
+++ app/src/main/java/com/teamosc/collegefests/NavigationDrawerFragment.java
@@ -97,15 +97,12 @@ public class NavigationDrawerFragment extends Fragment {
                 selectItem(position);
             }
         });
+        String[] drawerItems = getResources().getStringArray(R.array.drawer_items);
         mDrawerListView.setAdapter(new ArrayAdapter<String>(
                 getActionBar().getThemedContext(),
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
-                new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
-                }));
+                drawerItems));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
     }
